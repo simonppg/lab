07a706123750092bd78ab8bb43c284ec08a2e9f6
simonppg <simonppg@gmail.com>
2026-09-01 13:01:13 -0700

    phase 4: manage dataset ACLs

--- infrastructure/acl.tf
+++ infrastructure/acl.tf
@@ -47,3 +47,143 @@ resource "truenas_filesystem_acl" "mymedia" {
     }
   ]
 }
+
+resource "truenas_filesystem_acl" "jellyfin" {
+  path    = "/mnt/MyPool/Jellyfin"
+  acltype = "POSIX1E"
+
+  uid = 0
+  gid = 0
+
+  dacl = [
+    {
+      tag          = "USER_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "GROUP_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = false
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "OTHER"
+      id           = -1
+      perm_read    = true
+      perm_write   = false
+      perm_execute = true
+      default      = false
+    }
+  ]
+}
+
+resource "truenas_filesystem_acl" "jellyfin_cache" {
+  path    = "/mnt/MyPool/Jellyfin/Cache_Storage"
+  acltype = "POSIX1E"
+
+  uid = 568
+  gid = 568
+
+  dacl = [
+    {
+      tag          = "USER_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "GROUP_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "OTHER"
+      id           = -1
+      perm_read    = true
+      perm_write   = false
+      perm_execute = true
+      default      = false
+    }
+  ]
+}
+
+resource "truenas_filesystem_acl" "jellyfin_config" {
+  path    = "/mnt/MyPool/Jellyfin/Config_Storage"
+  acltype = "POSIX1E"
+
+  uid = 568
+  gid = 568
+
+  dacl = [
+    {
+      tag          = "USER_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "GROUP_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "OTHER"
+      id           = -1
+      perm_read    = true
+      perm_write   = false
+      perm_execute = true
+      default      = false
+    }
+  ]
+}
+
+resource "truenas_filesystem_acl" "jellyfin_transcode" {
+  path    = "/mnt/MyPool/Jellyfin/Transcode_Storage"
+  acltype = "POSIX1E"
+
+  uid = 568
+  gid = 568
+
+  dacl = [
+    {
+      tag          = "USER_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "GROUP_OBJ"
+      id           = -1
+      perm_read    = true
+      perm_write   = true
+      perm_execute = true
+      default      = false
+    },
+    {
+      tag          = "OTHER"
+      id           = -1
+      perm_read    = true
+      perm_write   = false
+      perm_execute = true
+      default      = false
+    }
+  ]
+}
--- infrastructure/users.tf
+++ infrastructure/users.tf
@@ -2,8 +2,8 @@ resource "truenas_user" "cinefilo" {
   username  = "cinefilo"
   full_name = "cinefilo"
 
-  uid   = 3000
-  group = truenas_group.cinefilo.id
+  uid    = 3000
+  group  = truenas_group.cinefilo.id
   groups = [91]
 
   home   = "/var/empty"
