bffc6567764479eeb31708b49156f10200520185
simonppg <simonppg@gmail.com>
2026-09-06 19:34:43 -0700

    Add environment loader for SOPS secrets

---
+++ scripts/load-secrets.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SECRETS_DIR="../secrets"
+
+# TrueNAS
+export TF_VAR_truenas_url="https://truenas.home.arpa"
+export TF_VAR_truenas_username="$(
+  sops -d --extract '["username"]' "$SECRETS_DIR/truenas.sops.yaml"
+)"
+export TF_VAR_truenas_api_key="$(
+  sops -d --extract '["api_key"]' "$SECRETS_DIR/truenas.sops.yaml"
+)"
+export TF_VAR_cinefilo_password="$(
+  sops -d --extract '["cinefilo_password"]' "$SECRETS_DIR/truenas.sops.yaml"
+)"
+export TF_VAR_truenas_ip="$(
+  sops -d --extract '["truenas_ip"]' "$SECRETS_DIR/truenas.sops.yaml"
+)"
+
+# BBB
+export TF_VAR_bbb_username="$(
+  sops -d --extract '["username"]' "$SECRETS_DIR/bbb.sops.yaml"
+)"
+export TF_VAR_bbb_password="$(
+  sops -d --extract '["password"]' "$SECRETS_DIR/bbb.sops.yaml"
+)"
+export TF_VAR_pihole_ip="$(
+  sops -d --extract '["pihole_ip"]' "$SECRETS_DIR/bbb.sops.yaml"
+)"
+
+echo "Environment loaded."
